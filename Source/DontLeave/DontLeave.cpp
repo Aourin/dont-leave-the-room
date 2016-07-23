d3a0da3720e7379b7b87d4e99e4c36b1f480b54a
Aourin <TheTitan8706@gmail.com>
2016-07-23 11:11:15 -0400

    init(base): base project
    Adds base project

---
+++ Source/DontLeave/DontLeave.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "DontLeave.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DontLeave, "DontLeave" );
