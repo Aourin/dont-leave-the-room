484f93157503c31e15c1d01041f98731eb4f7262
Aourin <TheTitan8706@gmail.com>
2016-07-31 22:44:51 -0400

    feat(Grabber): adds release function refactor(Grabber): updates grab with function refactor(Grabber): adds new player loc function and updates implementation

--- Source/DontLeave/Grabber.h
+++ Source/DontLeave/Grabber.h
@@ -5,6 +5,10 @@
 #include "Components/ActorComponent.h"
 #include "Grabber.generated.h"
 
+struct LocRot {
+	FVector Location;
+	FRotator Rotator;
+};
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class DONTLEAVE_API UGrabber : public UActorComponent
@@ -15,21 +19,29 @@ public:
 	// Sets default values for this component's properties
 	UGrabber();
 
+	AActor* TargetActor = nullptr;
 	// Called when the game starts
 	virtual void BeginPlay() override;
+
+	void FindInputHandler();
+	void FindPhysicsComponent();
 	
 	// Called every frame
 	virtual void TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction ) override;
 
+	void GetTargetOnRayTrace(FHitResult &Hit);
+
 private:
 	UPROPERTY(EditAnywhere)
-	AActor* TargetActor;
 	float Reach = 100.f;
 
 	UInputComponent* InputHandler = nullptr;
 	UPhysicsHandleComponent* PhysicsHandle = nullptr;
 
-
 	// Raycast and cast in reach
 	void Grab();
+	void Release();
+	const FVector GetReachPoint();
+	const FHitResult GetFirstPhysicsBodyInReach();
+	LocRot GetPlayerLocRot();
 };
